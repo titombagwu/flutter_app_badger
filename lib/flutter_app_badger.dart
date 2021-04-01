b52edb88c66ce4824078449a2a4ac6488cc4f827
Tito <tito@coverstory.co>
2021-04-01 15:06:58 -0400

    return futures

--- lib/flutter_app_badger.dart
+++ lib/flutter_app_badger.dart
@@ -6,16 +6,17 @@ class FlutterAppBadger {
   static const MethodChannel _channel =
       const MethodChannel('g123k/flutter_app_badger');
 
-  static void updateBadgeCount(int count) {
-    _channel.invokeMethod('updateBadgeCount', {"count": count});
+  static Future<void> updateBadgeCount(int count) {
+    return _channel.invokeMethod('updateBadgeCount', {"count": count});
   }
 
-  static void removeBadge() {
-    _channel.invokeMethod('removeBadge');
+  static Future<void> removeBadge() {
+    return _channel.invokeMethod('removeBadge');
   }
 
   static Future<bool> isAppBadgeSupported() async {
-    bool? appBadgeSupported = await _channel.invokeMethod('isAppBadgeSupported');
+    bool? appBadgeSupported =
+        await _channel.invokeMethod('isAppBadgeSupported');
     return appBadgeSupported ?? false;
   }
 }
